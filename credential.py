ef760077fab47c3cfcb36ed4c11eec11e964d405
reyzakomala <78785471+reyzakomala@users.noreply.github.com>
2021-02-16 00:43:07 +0700

    Add files via upload

---
+++ credential.py
@@ -0,0 +1,2 @@
+psuser = "dummy5.reyza@gmail.com"
+pspass = "dummy.reyza10"
---
+++ login-automation.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.action_chains import ActionChains
+import credential
+import time
+
+username = credential.psuser
+password = credential.pspass
+
+driver = webdriver.Chrome(executable_path='D:/Automation/chromedriver.exe')
+driver.get('https://wordpress.com/log-in')
+
+user_input = driver.find_element_by_css_selector('#usernameOrEmail')
+user_input.send_keys(username)
+
+driver.find_element_by_css_selector('#primary > div > main > div > div > form > div.card.login__form > div.login__form-action > button').click()
+
+password_input = driver.find_element_by_xpath('//*[@id="password"]')
+time.sleep(3)
+#password_input.send_keys(password)
+ActionChains(driver).move_to_element(password_input).send_keys(password).perform()
+time.sleep(3)
+
+driver.find_element_by_css_selector('#primary > div > main > div > div > form > div.card.login__form > div.login__form-action > button').click()
