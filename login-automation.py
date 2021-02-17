260ad26e81fb804665f96ed811c35dcf1a17e9d4
Reyza Pratama Komala <78785471+reyzakomala@users.noreply.github.com>
2021-02-17 20:08:20 +0700

    Update login-automation.py

--- login-automation.py
+++ login-automation.py
@@ -1,5 +1,4 @@
 from selenium import webdriver
-from selenium.webdriver.common.action_chains import ActionChains
 import credential
 import time
 
@@ -15,8 +14,7 @@ user_input.send_keys(username)
 driver.find_element_by_css_selector('#primary > div > main > div > div > form > div.card.login__form > div.login__form-action > button').click()
 
 password_input = driver.find_element_by_xpath('//*[@id="password"]')
-time.sleep(3)
-ActionChains(driver).move_to_element(password_input).send_keys(password).perform()
-time.sleep(3)
+driver.implicitly_wait(20)
+password_input.send_keys(password)
 
 driver.find_element_by_css_selector('#primary > div > main > div > div > form > div.card.login__form > div.login__form-action > button').click()
